a480f4787502dd26162469f433b897fb43566823
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-04-14 16:20:27 -0700

    testing

--- server.js
+++ server.js
@@ -12,20 +12,14 @@ client.connect();
 client.on('error', err => console.error(err));
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
-app.get('/tasks', (req, res) => {
-  client.query(`SELECT * from tasks;`)
+// app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
+app.get('/', (req, res) => {
+  client.query(`SELECT * from books;`)
     .then(results => res.send(results.rows))
     .catch(console.error);
 });
-
-
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
-
-
-
-
 
 
 
