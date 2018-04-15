882641752da96baf16f59929f6b1e99e52c07a47
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-04-14 17:16:56 -0700

    I'm tired

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const CLIENT_URL = 'https://11-production-deployment.github.io/book-list-client'
+const CLIENT_URL = 'https://11-production-deployment.github.io/book-list-client';
 
 
 const DATABASE_URL = 'postgres://skkthzllooexia:3c74caee8fec7aa7d47d583de9f927c2f8c056a73670add508730bfbd1d37d66@ec2-54-221-192-231.compute-1.amazonaws.com:5432/d6hmqdd984spkj';
